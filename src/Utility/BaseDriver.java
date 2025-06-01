4f9e5fea77493b894b3db9f077f0cda90ff232b4
Hilal Çelik <celikhilalx@gmail.com>
2025-06-01 08:41:33 +0300

    Update BaseDriver

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
 import java.time.Duration;
@@ -27,7 +28,7 @@ public class BaseDriver {
         actions = new Actions(driver);
     }
 
-    @AfterClass
+    @AfterMethod
     public void tearDown() {
         ElementActions.Wait(3);
         driver.quit();
