8d013af83ff9c6ac35af69c3e72f438635076c12
Hilal Çelik <celikhilalx@gmail.com>
2025-06-02 09:13:33 +0300

    Update US_407_DeletePatient test class

--- src/Tests/US_407_DeletePatient.java
+++ src/Tests/US_407_DeletePatient.java
@@ -4,22 +4,21 @@ import Pages.US_407_408_410_POM;
 import Utility.LoggedInBaseTest;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class US_407_DeletePatient extends LoggedInBaseTest {
 
-    @Test(dataProvider = "patientInformation")
-    public void deletePatient(String patient) {
+    @Test
+    public void deletePatient() {
 
         US_407_408_410_POM elements = new US_407_408_410_POM();
 
         elements.findPatientRecordButton.click();
-        elements.searchInput.sendKeys(patient);
+        elements.searchInput.sendKeys("1000NG");
 
         try {
             wait.until(ExpectedConditions.visibilityOf(elements.noMatchMessage));
-            System.out.println("Hasta bulunamadı: " + patient);
+            System.out.println("Hasta bulunamadı: " + "1000NG");
             return;
         } catch (Exception e) {
             System.out.println("Hasta siliniyor...");
@@ -31,10 +30,5 @@ public class US_407_DeletePatient extends LoggedInBaseTest {
         elements.confirmButton.click();
 
         Assert.assertTrue(elements.deleteSuccessMessage.getText().contains("deleted"));
-    }
-
-    @DataProvider
-    Object[] patientInformation() {
-        return new Object[]{"1000NG"};
     }
 }
